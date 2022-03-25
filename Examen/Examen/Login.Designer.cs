fd2e1fb32642b3cc1f2910fba2c72eea6409176e
KateAndara <98791109+KateAndara@users.noreply.github.com>
2022-03-25 15:58:48 -0600

    Avance
    Avance

---
+++ Examen/Examen/Login.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace Examen
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.UsuariotextBox = new System.Windows.Forms.TextBox();
+            this.ClavetextBox = new System.Windows.Forms.TextBox();
+            this.Aceptarbutton = new System.Windows.Forms.Button();
+            this.Cancelarbutton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(283, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "LOGIN";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(41, 138);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 32);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Clave:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 74);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(130, 32);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Usuario:";
+            // 
+            // UsuariotextBox
+            // 
+            this.UsuariotextBox.Location = new System.Drawing.Point(148, 80);
+            this.UsuariotextBox.Name = "UsuariotextBox";
+            this.UsuariotextBox.Size = new System.Drawing.Size(394, 26);
+            this.UsuariotextBox.TabIndex = 3;
+            this.UsuariotextBox.Text = "KANDARA";
+            // 
+            // ClavetextBox
+            // 
+            this.ClavetextBox.Location = new System.Drawing.Point(148, 144);
+            this.ClavetextBox.Name = "ClavetextBox";
+            this.ClavetextBox.PasswordChar = '*';
+            this.ClavetextBox.Size = new System.Drawing.Size(394, 26);
+            this.ClavetextBox.TabIndex = 4;
+            this.ClavetextBox.Text = "123456";
+            // 
+            // Aceptarbutton
+            // 
+            this.Aceptarbutton.BackColor = System.Drawing.Color.Turquoise;
+            this.Aceptarbutton.Font = new System.Drawing.Font("Arial Rounded MT Bold", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Aceptarbutton.Location = new System.Drawing.Point(148, 207);
+            this.Aceptarbutton.Name = "Aceptarbutton";
+            this.Aceptarbutton.Size = new System.Drawing.Size(132, 39);
+            this.Aceptarbutton.TabIndex = 5;
+            this.Aceptarbutton.Text = "Aceptar";
+            this.Aceptarbutton.UseVisualStyleBackColor = false;
+            this.Aceptarbutton.Click += new System.EventHandler(this.Aceptarbutton_Click);
+            // 
+            // Cancelarbutton
+            // 
+            this.Cancelarbutton.BackColor = System.Drawing.Color.Turquoise;
+            this.Cancelarbutton.Font = new System.Drawing.Font("Arial Rounded MT Bold", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Cancelarbutton.Location = new System.Drawing.Point(410, 207);
+            this.Cancelarbutton.Name = "Cancelarbutton";
+            this.Cancelarbutton.Size = new System.Drawing.Size(132, 39);
+            this.Cancelarbutton.TabIndex = 6;
+            this.Cancelarbutton.Text = "Cancelar";
+            this.Cancelarbutton.UseVisualStyleBackColor = false;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.NavajoWhite;
+            this.ClientSize = new System.Drawing.Size(631, 258);
+            this.Controls.Add(this.Cancelarbutton);
+            this.Controls.Add(this.Aceptarbutton);
+            this.Controls.Add(this.ClavetextBox);
+            this.Controls.Add(this.UsuariotextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Login";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox UsuariotextBox;
+        private System.Windows.Forms.TextBox ClavetextBox;
+        private System.Windows.Forms.Button Aceptarbutton;
+        private System.Windows.Forms.Button Cancelarbutton;
+    }
+}
+
---
+++ Examen/Examen/Login.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Datos;
+using Datos.Accesos;
+using Datos.Entidades;
+
+namespace Examen
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Aceptarbutton_Click(object sender, EventArgs e)
+        {
+            UsuarioDA usuarioDA = new UsuarioDA();
+            Usuario usuario = new Usuario();
+            usuario = usuarioDA.Login(UsuariotextBox.Text, ClavetextBox.Text);
+
+            if (usuario == null)
+            {
+                MessageBox.Show("Datos erroneos");
+                return;
+            }
+            else if (!usuario.EstaActivo)
+            {
+                MessageBox.Show("Usuario Inactivo");
+                return;
+            }
+            FrmProducto frmproducto = new FrmProducto();
+            frmproducto.Show();
+        }
+    }
+}
